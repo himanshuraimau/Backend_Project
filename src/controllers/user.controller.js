d9b278408e64b01dddfeceb10730679c6f818ced
Himanshu Rai <1si23ad016@sit.ac.in>
2024-03-28 22:37:35 +0530

    mistake

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async(res,req)=>{
+const registerUser = asyncHandler(async(req,res)=>{
     res.status(200).json({
-        message:"Ok"
+        message:"himanshu"
     })
 })
-
-
 
 export {registerUser};
