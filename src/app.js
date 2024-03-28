e01dd2f5275a13d8e52f7270e547bfa123895308
Himanshu Rai <1si23ad016@sit.ac.in>
2024-03-28 22:27:00 +0530

    "User COntroller "

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -15,6 +15,14 @@ app.use(express.json({limit:"16kb"}))
 
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
-app.use(express.cookieParser())
+app.use(cookieParser())
+
+//ROUTES import
+import userRouter from "./routes/user.routes.js";
+
+//ROutes declartion
+app.use("/api/v1/users",userRouter)
+//https://localhost:8000/api/v1/users/register
+
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { json } from "stream/consumers";
+import {app} from "./app.js";
+
 
 dotenv.config({
     path: "./env"
